4e0c2ca3e708d38746bce9a38d4b560477c07bc0
Vikas Kumar <80447331+vikasOzark@users.noreply.github.com>
2024-01-14 12:59:17 +0530

    formated

--- DAY-1-Trebuchet/practice.py
+++ DAY-1-Trebuchet/practice.py
@@ -1,6 +1,7 @@
 # from . import question_input
 from question_input import QUESTION_INPUT, sample_string_two, number_map, identifier
 
+
 class AdventDayOneTrebuchet:
     "This class is used to solve the advent of code question Day 1: Trebuchet"
 
@@ -25,13 +26,13 @@ class AdventDayOneTrebuchet:
         :RETURNS
             returns the sum of the combined string numbers.
         """
-        
+
         if not self.is_number_contains():
             return False
 
-        
         for string in self.input_string:
-            number_string: str = [num for num in list(string) if num.isnumeric()]
+            number_string: str = [
+                num for num in list(string) if num.isnumeric()]
             combined_digit = str(number_string[0]) + str(number_string[-1])
             print(f" {string} -> {combined_digit}")
             self.combined_numbers.append(int(combined_digit))
@@ -41,27 +42,30 @@ class AdventDayOneTrebuchet:
     def solution_two(self):
         string_array = self.input_string.splitlines()
         results = []
-        
+
         for index, string in enumerate(string_array):
             final_string = string
             idx = 1
             while idx <= len(final_string):
-                current_string = final_string[idx -1]
-                
+                current_string = final_string[idx - 1]
+
                 filtered_list = filtered_list = list(
-                    filter(lambda val: val.startswith(current_string), identifier)
+                    filter(lambda val: val.startswith(
+                        current_string), identifier)
                 )
-                
+
                 if len(filtered_list) > 0:
-                    
+
                     for item in filtered_list:
                         item_length = len(item)
-                        
-                        check_string = final_string[ idx - 1 : (idx - 1) + item_length]
+
+                        check_string = final_string[idx -
+                                                    1: (idx - 1) + item_length]
                         num_letter = number_map.get(check_string)
-                        
+
                         if num_letter:
-                            stripped_string = final_string[((idx - 1) + item_length):]
+                            stripped_string = final_string[(
+                                (idx - 1) + item_length):]
                             rest_string = final_string[:(idx - 1)]
                             final_string = rest_string + num_letter + stripped_string
                 idx += 1
@@ -69,8 +73,9 @@ class AdventDayOneTrebuchet:
             results.append(final_string)
         self.input_string = results
         return results
-    
-if __name__ == "__main__" :
+
+
+if __name__ == "__main__":
     solution = AdventDayOneTrebuchet(QUESTION_INPUT)
     solution.solution_two()
     total, li = solution.calculate_result()
